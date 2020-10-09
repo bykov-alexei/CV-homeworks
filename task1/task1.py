36d8f8933ab87b0ce66ac7153cc935760ade2b5a
Alexei <bitskoff@gmail.com>
2020-10-09 18:32:52 +0800

    updated task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -1,10 +1,18 @@
 from io import StringIO
 import numpy as np
 
-filename = 'figure6.txt'
-with open(filename, 'r') as f:
-    size = float(f.readline())
-    f.readline()
-    img = np.loadtxt(StringIO(f.read()))
-pic_width = img.shape[1]
-print(round(size / pic_width, 4))
+def read(filename):
+    with open(filename, 'r') as f:
+        size = float(f.readline())
+        f.readline()
+        img = np.loadtxt(StringIO(f.read()))
+    return size, img  
+
+filename = input()
+size, img = read(filename)
+y, x = np.where(img == 1)
+if len(x) == 0:
+    print('No object on the image')
+else:
+    width = np.max(x) - np.min(x)
+    print(round(size / width, 4))
