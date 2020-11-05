da53a6bba3dfd7447b1427fbe546ab4b4baea4f8
Alexei <mail@bykov-alexei.ru>
2020-11-05 12:01:53 +0800

    task4-fixed

--- task4/task4.py
+++ task4/task4.py
@@ -3,7 +3,19 @@ import matplotlib.pyplot as plt
 from skimage import morphology, measure
 from skimage.color import rgb2hsv
 
+def find_cluster(clusters, color):
+    return min(clusters, key=lambda x: abs(color - x))
+
+def clusterize(colors):
+    clusters = [colors[0]]
+    for color in colors[1:]:
+        d = abs(color - find_cluster(clusters, color))
+        if d > 0.05:
+            clusters.append(color)
+    return clusters
+
 img = plt.imread('balls_and_rects.png')[::, ::, :3]
+hsv = rgb2hsv(img)
 gs = img.sum(axis=2)
 gs[gs > 0] = 1
 lb = measure.label(gs)
@@ -14,16 +26,18 @@ figures = {
 }
 
 regions = measure.regionprops(lb)
+clusters = clusterize(np.unique(hsv[:, :, 0]))
 for region in regions:
     cx, cy = map(int, region.centroid)
-    color = round(rgb2hsv(img[cx, cy])[0], 1)
+    color = hsv[cx, cy][0]
     figure = 'circle'
     if region.extent == 1:
         figure = 'rectangle'
-    if color in figures[figure]:
-        figures[figure][color] += 1
+    cluster = find_cluster(clusters, color)
+    if cluster in figures[figure]:
+        figures[figure][cluster] += 1
     else:
-        figures[figure][color] = 1
+        figures[figure][cluster] = 1
 
 print("Number of figures", len(regions))
 squares = figures['rectangle']
@@ -31,8 +45,9 @@ circles = figures['circle']
 print("Number of Squares", sum(squares.values()))
 print("Number of Circles", sum(circles.values()))
 print("Squares:")
+
 for color in squares:
-    print("Color: %s" % str(color), "Number: ", squares[color])
+    print("Color: %s" % str(round(color, 2)), "Number: ", squares[color])
 print("Circles:")
 for color in circles:
-    print("Color: %s" % str(color), "Number: ", circles[color])
+    print("Color: %s" % str(round(color, 2)), "Number: ", circles[color])
